83e6ba977953940ac741f06b31d74a3d721f7147
Charles Rudder <crudder@citiesthatwork.com>
2020-09-14 18:06:45 -0400

    updated summarize data to calculate buidlout and convert to Jobs and DUs

--- python/summarize_data.py
+++ python/summarize_data.py
@@ -5,16 +5,46 @@ import numpy as np
 
 arcpy.env.overwriteOutput = True
 
+# Use groupings
+RES = ["SF", "MF"]
+NRES = ["Ret", "Ind", "Off"]
+HOTEL = ["Hot"]
+UNTRACKED = ["Oth"]
+USES = RES + NRES + HOTEL + UNTRACKED
+
+# share values
+shares = {"SF": 1800,
+          "MF": 1200,
+          "Ret": 500,
+          "Ind": 800,
+          "Off": 400,
+          "Hot": 500}
+
+
+# support function
+def genFieldList(suffix, measure='SF', include_untracked=True):
+    """
+    Generates a list of fields based on use groupings with the form:
+    `{UseGroup}_SF_{suffix}`, recording the square footage (SF) for each
+    use grouping for a time frame indicated by the suffix.
+    """
+    global USES, UNTRACKED
+    if include_untracked:
+        return ["{}_{}_{}".format(use, measure, suffix) for use in USES]
+    else:
+        return ["{}_{}_{}".format(use, measure, suffix) for use in USES if use not in UNTRACKED]
+
+
 # workspaces
 scenario_gdb = r'K:\Projects\BCDCOG\Features\Files_For_RDB\RDB_V3\scenarios\WE_Sum\WE_Sum_scenario.gdb'
 source_gdb = r'K:\Projects\BCDCOG\Features\Files_For_RDB\RDB_V3\LCBRT_data.gdb'
-summary_gdb = r'K:\Projects\BCDCOG\Features\Files_For_RDB\RDB_V3\scenarios\WE_Sum\summaries.gdb'
+summary_wkspc = r'K:\Projects\BCDCOG\Features\Files_For_RDB\RDB_V3\scenarios\WE_Sum'
 
 # datasets
 parcels = path.join(scenario_gdb, 'parcels')
 pid = 'ParclID'
-p_fields = [pid] + ['SF_SF_ExPi', 'MF_SF_ExPi', 'Ret_SF_ExPi',
-                  'Ind_SF_ExPi', 'Off_SF_ExPi', 'Hot_SF_ExPi']
+expi_flds = genFieldList(suffix='ExPi', include_untracked=False)
+p_fields = [pid] + expi_flds
 
 taz = path.join(source_gdb, 'corridor_segments_byTAZ')
 tid = 'Big_TAZ'
@@ -24,15 +54,15 @@ sid = 'SegmentNum'
 
 capacities = path.join(scenario_gdb, 'capacity')
 cid = pid
-c_fields = [cid] + ['SF_SF_TotCap', 'MF_SF_TotCap', 'Ret_SF_TotCap',
-                    'Ind_SF_TotCap', 'Off_SF_TotCap', 'Hot_SF_TotCap']
+totcap_flds = genFieldList(suffix='TotCap', include_untracked=False)
+c_fields = [cid] + totcap_flds
 
 allocation = path.join(scenario_gdb, 'allocation')
 aid = pid
-a_fields = [aid] + ['SF_SF_alloc', 'MF_SF_alloc', 'Ret_SF_alloc',
-                    'Ind_SF_alloc', 'Off_SF_alloc', 'Hot_SF_alloc','seg_num']
+alloc_flds = genFieldList(suffix='alloc', include_untracked=False)
+a_fields = [aid, 'seg_num'] + alloc_flds
 
-
+buildout_flds = genFieldList(suffix="2040", include_untracked=False)
 '''
 work to do:
 1) sj parcels to TAZ to get TAZID with parcel
@@ -47,7 +77,7 @@ work to do:
 # arcpy.CreateFileGDB_management(out_folder_path=path, out_name=gdb)
 
 p_fl = arcpy.MakeFeatureLayer_management(in_features=parcels, out_layer='parcels_fl')
-pwTAZ = arcpy.SpatialJoin_analysis(parcels, taz, path.join(summary_gdb, 'parcels_wTAZ'), match_option="INTERSECT")
+pwTAZ = arcpy.SpatialJoin_analysis(parcels, taz, 'in_memory\parcels_wTAZ', match_option="INTERSECT")
 
 p_df = pd.DataFrame(
     arcpy.da.TableToNumPyArray(
@@ -62,23 +92,33 @@ alloc_df = pd.DataFrame(
 # join all tables together and summarize
 joined = (p_df.join(cap_df)).join(alloc_df)
 seg_summaries = joined.groupby('seg_num').sum()
-seg_summaries.drop(tid, axis=1)
+seg_summaries.drop(tid, axis=1, inplace=True)
+for expi_fld, alloc_fld, buildout_fld in zip(expi_flds, alloc_flds, buildout_flds):
+    seg_summaries[buildout_fld] = seg_summaries[expi_fld] + seg_summaries[alloc_fld]
+
+seg_summaries['RES'] = (seg_summaries[buildout_flds[0]] / shares['SF']) + (seg_summaries[buildout_flds[1]] / shares['MF'])
+seg_summaries['JOBS'] = (seg_summaries[buildout_flds[2]] / shares['Ret']) + (seg_summaries[buildout_flds[3]] / shares['Ind']) + (seg_summaries[buildout_flds[4]] / shares['Off']) + (seg_summaries[buildout_flds[5]] / shares['Hot'])
+
 taz_summaries = joined.groupby(tid).sum()
-taz_summaries.drop('seg_num', axis=1)
+taz_summaries.drop('seg_num', axis=1, inplace=True)
+for expi_fld, alloc_fld, buildout_fld in zip(expi_flds, alloc_flds, buildout_flds):
+    taz_summaries[buildout_fld] = taz_summaries[expi_fld] + taz_summaries[alloc_fld]
+taz_summaries['RES'] = (taz_summaries[buildout_flds[0]] / shares['SF']) + (taz_summaries[buildout_flds[1]] / shares['MF'])
+taz_summaries['JOBS'] = (taz_summaries[buildout_flds[2]] / shares['Ret']) + (taz_summaries[buildout_flds[3]] / shares['Ind']) + (taz_summaries[buildout_flds[4]] / shares['Off']) + (taz_summaries[buildout_flds[5]] / shares['Hot'])
+
 
 # write out tables
-pth, gdb = path.split(summary_gdb)
-taz_summaries.to_csv(path.join(pth, 'taz_summary.csv'))
-seg_summaries.to_csv(path.join(pth, 'seg_summary.csv'))
+taz_summaries.to_csv(path.join(summary_wkspc, 'taz_summary.csv'))
+seg_summaries.to_csv(path.join(summary_wkspc, 'seg_summary.csv'))
 
-seg_sum_tbl = path.join(summary_gdb, 'segment_summaries')
-out_array = np.array(np.rec.fromrecords(seg_summaries.values))
-names = seg_summaries.dtypes.index.tolist()
-out_array.dtype.names = tuple(names)
-arcpy.da.NumPyArrayToTable(out_array, seg_sum_tbl)
-
-taz_sum_tbl = path.join(summary_gdb, 'taz_summaries')
-out_array = np.array(np.rec.fromrecords(taz_summaries.values))
-names = taz_summaries.dtypes.index.tolist()
-out_array.dtype.names = tuple(names)
-arcpy.da.NumPyArrayToTable(out_array, taz_sum_tbl)
+# seg_sum_tbl = path.join(summary_gdb, 'segment_summaries')
+# out_array = np.array(np.rec.fromrecords(seg_summaries.values))
+# names = seg_summaries.dtypes.index.tolist()
+# out_array.dtype.names = tuple(names)
+# arcpy.da.NumPyArrayToTable(out_array, seg_sum_tbl)
+#
+# taz_sum_tbl = path.join(summary_gdb, 'taz_summaries')
+# out_array = np.array(np.rec.fromrecords(taz_summaries.values))
+# names = taz_summaries.dtypes.index.tolist()
+# out_array.dtype.names = tuple(names)
+# arcpy.da.NumPyArrayToTable(out_array, taz_sum_tbl)
