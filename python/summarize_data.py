6a75f3a97db3967effe3402cd7919e5e3af865a4
charles rudder <crudder@citiesthatwork.com>
2020-09-23 17:47:06 -0400

    modify summarize_data to function and make callable in scenario script

--- python/summarize_data.py
+++ python/summarize_data.py
@@ -57,62 +57,61 @@ segments = path.join(source_gdb, "corridor_segments")
 sid = "SegmentNum"
 
 
-"""
-work to do:
-1) sj parcels to TAZ to get TAZID with parcel
-2) join cap and allocation to parcels
-3) summarize by Segment
-4) summariZe by TAZ
-"""
 # pth, gdb = path.split(summary_gdb)
 # if arcpy.Exists(summary_gdb):
 #     arcpy.Delete_management(in_data=summary_gdb)
 #
 # arcpy.CreateFileGDB_management(out_folder_path=path, out_name=gdb)
 
-p_fl = arcpy.MakeFeatureLayer_management(in_features=parcels, out_layer="parcels_fl")
-pwTAZ = arcpy.SpatialJoin_analysis(
-    parcels, taz, "in_memory\parcels_wTAZ", match_option="INTERSECT"
-)
-
-p_df = pd.DataFrame(
-    arcpy.da.TableToNumPyArray(
-        in_table=pwTAZ, field_names=p_fields + [tid], null_value=0.0
+def generate_summaries(suit_fc, suit_id, suit_fields, taz_fc, taz_id, seg_id)
+    # setup field lists
+    expi_flds = genFieldList(suffix="ExPi", include_untracked=False)
+    totcap_flds = genFieldList(suffix="TotCap", include_untracked=False)
+    alloc_flds = genFieldList(suffix="alloc", include_untracked=False)
+    buildout_flds = genFieldList(suffix="2040", include_untracked=False)
+    
+    pwTAZ = arcpy.SpatialJoin_analysis(
+        suti_fc, taz_fc, "in_memory\parcels_wTAZ", match_option="INTERSECT"
     )
-).set_index(pid)
 
-seg_summaries = p_df.groupby("seg_num").sum()
-seg_summaries.drop(tid, axis=1, inplace=True)
-for expi_fld, alloc_fld, buildout_fld in zip(expi_flds, alloc_flds, buildout_flds):
-    seg_summaries[buildout_fld] = seg_summaries[expi_fld] + seg_summaries[alloc_fld]
+    p_df = pd.DataFrame(
+        arcpy.da.TableToNumPyArray(
+            in_table=pwTAZ, field_names=suit_fields + [taz_id], null_value=0.0
+        )
+    ).set_index(suit_id)
 
-seg_summaries["RES"] = (seg_summaries[buildout_flds[0]] / shares["SF"]) + (
-    seg_summaries[buildout_flds[1]] / shares["MF"]
-)
-seg_summaries["JOBS"] = (
-    (seg_summaries[buildout_flds[2]] / shares["Ret"])
-    + (seg_summaries[buildout_flds[3]] / shares["Ind"])
-    + (seg_summaries[buildout_flds[4]] / shares["Off"])
-    + (seg_summaries[buildout_flds[5]] / shares["Hot"])
-)
+    seg_summaries = p_df.groupby(seg_id).sum()
+    seg_summaries.drop(tid, axis=1, inplace=True)
+    for expi_fld, alloc_fld, buildout_fld in zip(expi_flds, alloc_flds, buildout_flds):
+        seg_summaries[buildout_fld] = seg_summaries[expi_fld] + seg_summaries[alloc_fld]
 
-taz_summaries = p_df.groupby(tid).sum()
-taz_summaries.drop("seg_num", axis=1, inplace=True)
-for expi_fld, alloc_fld, buildout_fld in zip(expi_flds, alloc_flds, buildout_flds):
-    taz_summaries[buildout_fld] = taz_summaries[expi_fld] + taz_summaries[alloc_fld]
-taz_summaries["RES"] = (taz_summaries[buildout_flds[0]] / shares["SF"]) + (
-    taz_summaries[buildout_flds[1]] / shares["MF"]
-)
-taz_summaries["JOBS"] = (
-    (taz_summaries[buildout_flds[2]] / shares["Ret"])
-    + (taz_summaries[buildout_flds[3]] / shares["Ind"])
-    + (taz_summaries[buildout_flds[4]] / shares["Off"])
-)  # + (taz_summaries[buildout_flds[5]] / shares['Hot'])
+    seg_summaries["RES"] = (seg_summaries[buildout_flds[0]] / shares["SF"]) + (
+        seg_summaries[buildout_flds[1]] / shares["MF"]
+    )
+    seg_summaries["JOBS"] = (
+        (seg_summaries[buildout_flds[2]] / shares["Ret"])
+        + (seg_summaries[buildout_flds[3]] / shares["Ind"])
+        + (seg_summaries[buildout_flds[4]] / shares["Off"])
+        + (seg_summaries[buildout_flds[5]] / shares["Hot"])
+    )
+
+    taz_summaries = p_df.groupby(tid).sum()
+    taz_summaries.drop("seg_num", axis=1, inplace=True)
+    for expi_fld, alloc_fld, buildout_fld in zip(expi_flds, alloc_flds, buildout_flds):
+        taz_summaries[buildout_fld] = taz_summaries[expi_fld] + taz_summaries[alloc_fld]
+    taz_summaries["RES"] = (taz_summaries[buildout_flds[0]] / shares["SF"]) + (
+        taz_summaries[buildout_flds[1]] / shares["MF"]
+    )
+    taz_summaries["JOBS"] = (
+        (taz_summaries[buildout_flds[2]] / shares["Ret"])
+        + (taz_summaries[buildout_flds[3]] / shares["Ind"])
+        + (taz_summaries[buildout_flds[4]] / shares["Off"])
+    )  # + (taz_summaries[buildout_flds[5]] / shares['Hot'])
 
 
-# write out tables
-taz_summaries.to_csv(path.join(summary_wkspc, "taz_summary.csv"))
-seg_summaries.to_csv(path.join(summary_wkspc, "seg_summary.csv"))
+    # write out tables
+    taz_summaries.to_csv(path.join(summary_wkspc, "taz_summary.csv"))
+    seg_summaries.to_csv(path.join(summary_wkspc, "seg_summary.csv"))
 
 # seg_sum_tbl = path.join(summary_gdb, 'segment_summaries')
 # out_array = np.array(np.rec.fromrecords(seg_summaries.values))
